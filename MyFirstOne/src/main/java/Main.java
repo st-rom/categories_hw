c0e135d4318c07b5bc6a8d9819f70d55281ed94b
st-rom <r.stepaniuk@ucu.edu.ua>
2017-09-26 04:44:00 +0300

    Add files via upload

--- MyFirstOne/src/main/java/Main.java
+++ MyFirstOne/src/main/java/Main.java
@@ -2,7 +2,10 @@ import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 class Main{
@@ -11,29 +14,31 @@ class Main{
         parse_category(url);
     }
     private static void parse_category(String url) throws IOException{
-        //Document doc = Jsoup.connect("https://rozetka.com.ua/ua/mobile-cases/c146229/filter/page=1").get();
-        //System.out.print(doc);
-        //Elements ss = doc.select("title");
-        //String first = ss.text().substring(0, 30);
-        //System.out.print("\n" + first);
-        //Elements em = doc.select("a[class*=paginator-catalog-l-link]");
-        //Elements tiles = doc.select("div[class*=g-i-tile-i-title]");
+        StringBuilder sb = new StringBuilder();
+        int maxim = 0;
+        int maxtry;
+        String inform = "";
         for(int i = 1; i < 6; i++){
             Document doc = Jsoup.connect("https://rozetka.com.ua/ua/mobile-cases/c146229/filter/page=" +
                     Integer.toString(i)).get();
             Elements tiles = doc.select("div[class*=g-i-tile-i-title]");
             for(Element tile:tiles) {
-                //System.out.print(tile);
                 Elements nah = tile.select("a[href]");
                 String op = nah.attr("href");
-                System.out.print("\n" + tile.text() + parse_reviews(op + "comments/"));
-                //System.out.print("\n" + "");
-                //Elements as = tile.select("a[class*=href]");// + "comments/");
-
-                //System.out.print("\n" + op + "comment/");
-
+                String parser = parse_reviews(op + "comments/");
+                maxtry = Integer.parseInt(parser.substring(0, parser.indexOf(" ")));
+                if(maxtry > maxim){
+                    maxim = maxtry;
+                    inform = "\n" + tile.text() + " " + parser;
+                }
+                System.out.print("\n" + tile.text() + " " + parser);
+                sb.append(tile.text() + " " + parser + "\n");
             }
         }
+        PrintWriter pw = new PrintWriter(new File("cases.csv"));
+        pw.write(sb.toString());
+        pw.close();
+        System.out.print("\n*********\nMax number of reviews had:" + inform);
     }
     private static String parse_reviews(String url) throws IOException{
         Document meg = Jsoup.connect(url).get();
@@ -43,22 +48,19 @@ class Main{
             num += 1;
             //System.out.print(n);
         }
-        String []sents = new String[num];
         int nu;
         int coms = 0;
         for(nu = 1; nu < num + 1; nu++){
             String pg = url + "page=" + nu + "/";
             //System.out.print("\n" + pg);
-            //sents[nu - 1] = parse_reviews_page(pg);
             coms += parse_reviews_page(pg);
 
         }
         //System.out.print("\n" + coms + " reviews from " + url);
-        String kst = "\n" + coms + " reviews from " + url;
-        return kst;
-
-
+        return coms + " reviews from " + url;
     }
+
+
     private  static int parse_reviews_page(String url) throws IOException{
         Document sup = Jsoup.connect(url).get();
         Elements reviews = sup.select("article[class*=pp-review-i]");
